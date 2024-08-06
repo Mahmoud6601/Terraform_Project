0016d94cd4bc5233dc321605cf4437588813ab64
Mahmoud Salah <77101374+Mahmoud6601@users.noreply.github.com>
2024-08-06 08:55:04 +0300

    Update variables.tf

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -1,1 +1,30 @@
+variable "names" {
+  description = "List of names for the instances"
+  type        = list(string)
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key name for the EC2 instance"
+  type        = string
+}
+
+variable "monitoring" {
+  description = "Whether detailed monitoring is enabled"
+  type        = bool
+}
+
+variable "vpc_security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the instance"
+  type        = string
+}
 

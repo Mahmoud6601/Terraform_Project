596bf01f6e12e44120ca236dbc498bf9434ecd8c
Mahmoud Salah <77101374+Mahmoud6601@users.noreply.github.com>
2024-08-06 09:06:35 +0300

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "name" {
+  description = "Name for the VPC"
+  type        = string
+}

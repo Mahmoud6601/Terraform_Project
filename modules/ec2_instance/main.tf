fc8b244d22b235584e11a99708e091f0ac47d0db
Mahmoud Salah <77101374+Mahmoud6601@users.noreply.github.com>
2024-08-06 08:54:46 +0300

    Update main.tf

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,1 +1,16 @@
+resource "aws_instance" "this" {
+  count         = length(var.names)
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  monitoring    = var.monitoring
+
+  vpc_security_group_ids = var.vpc_security_group_ids
+  subnet_id              = var.subnet_id
+
+  tags = {
+    Name = "instance-${var.names[count.index]}"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
 

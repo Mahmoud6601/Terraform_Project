488b819299b16794aa8c682c89d2129acc8a7d4e
Mahmoud Salah <77101374+Mahmoud6601@users.noreply.github.com>
2024-08-06 08:24:23 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.41.0"
     }
   }
 }
 
 provider "aws" {
-  
+  region = "us-west-2"  # Replace with your desired AWS region
+  # You can also specify access credentials here if not using environment variables
+  # access_key = "your_access_key"
+  # secret_key = "your_secret_key"
 }
